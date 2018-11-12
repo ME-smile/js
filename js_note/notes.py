1b032c8fee974cece661527abfa2bdd4a49f5c3b
ME-smile <demonisnotevil@163.com>
2018-11-12 10:29:09 +0800

    this repository is for js study

---
+++ js_note/notes.py
@@ -0,0 +1,42 @@
+###import 规范 pythoon  自带  第三方 自定义  空行分离
+#Automatically set the field to now every time the object is saved.  auto_now
+#Automatically set the field to now when the object is first created. auto_now_add
+#facilitate
+#populate
+#duration
+#
+#
+#user表设计
+#   用户名 username max_length  20
+#   密码 pwd  
+#   头像  portrait  Imge  max 100
+#   生日 blank
+#   性别
+#   
+#验证码
+#
+#
+#blogs
+#   title
+#   author
+#   content
+#   
+#   created_time
+#   categeroy(类别)
+#   updated_time
+#     
+#category
+#   blog(外键)
+#   
+#   
+#user_operation
+#   blog(外键)
+#   favor_nums(收藏)
+#   click_nums(阅读量)
+#   thumb_up(点赞量)
+#   comment(评论)
+#   
+#   
+#   
+#
+#   
